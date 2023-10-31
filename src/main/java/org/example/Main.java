a12715130ab60b3254cb4c5d95aba39d896f7671
GATA5 <Tangjing.Gan@coop.ch>
2023-10-31 13:13:17 +0100

    Setup Git

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int zahl = 7;
+        System.out.println(geradeZahl(zahl));
+    }
+
+    public static int geradeZahl(int zahl) {
+        System.out.println(zahl);
+
+        if (zahl % 2 == 0) {
+            return zahl;
+        } else {
+            return zahl * 2;
+
+
+        }
+
+    }
+}
+
